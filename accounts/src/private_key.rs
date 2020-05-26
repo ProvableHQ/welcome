70c97cfa340297114109ad118daa173f8eb15fa0
howardwu <howardwu@berkeley.edu>
2020-05-26 00:49:45 -0700

    Updates private and public key generation to be more efficient

--- accounts/src/private_key.rs
+++ accounts/src/private_key.rs
@@ -1,6 +1,6 @@
 use crate::errors::PrivateKeyError;
 
-use snarkos_dpc::base_dpc::{instantiated::Components, parameters::PublicParameters};
+use snarkos_dpc::base_dpc::{instantiated::Components, parameters::CircuitParameters};
 use snarkos_objects::AccountPrivateKey;
 
 use rand::{CryptoRng, Rng};
@@ -12,17 +12,14 @@ pub struct PrivateKey {
 
 impl PrivateKey {
     pub fn new<R: Rng + CryptoRng>(metadata: Option<[u8; 32]>, rng: &mut R) -> Result<Self, PrivateKeyError> {
-        let mut parameters_path = std::env::current_dir()?;
-        parameters_path.push("../snarkos/dpc/src/parameters/");
-        let parameters = PublicParameters::<Components>::load(&parameters_path, true)?;
-
         // Resolve the metadata value
         let metadata = match metadata {
             Some(metadata) => metadata,
             None => [0u8; 32],
         };
 
-        let private_key = AccountPrivateKey::<Components>::new(parameters.account_signature_parameters(), &metadata, rng)?;
+        let parameters = CircuitParameters::<Components>::load()?;
+        let private_key = AccountPrivateKey::<Components>::new(&parameters.account_signature, &metadata, rng)?;
         Ok(Self { private_key })
     }
 
--- accounts/src/public_key.rs
+++ accounts/src/public_key.rs
@@ -1,6 +1,6 @@
 use crate::{errors::PublicKeyError, private_key::PrivateKey};
 
-use snarkos_dpc::base_dpc::{instantiated::Components, parameters::PublicParameters};
+use snarkos_dpc::base_dpc::{instantiated::Components, parameters::CircuitParameters};
 use snarkos_objects::AccountPublicKey;
 use snarkos_utilities::bytes::ToBytes;
 
@@ -12,12 +12,8 @@ pub struct PublicKey {
 
 impl PublicKey {
     pub fn from(private_key: &PrivateKey) -> Result<Self, PublicKeyError> {
-        let mut parameters_path = std::env::current_dir()?;
-        parameters_path.push("../snarkos/dpc/src/parameters/");
-        let parameters = PublicParameters::<Components>::load(&parameters_path, true)?;
-
-        let public_key = AccountPublicKey::<Components>::from(parameters.account_commitment_parameters(), private_key.to_inner_ref())?;
-
+        let parameters = CircuitParameters::<Components>::load()?;
+        let public_key = AccountPublicKey::<Components>::from(&parameters.account_commitment, private_key.to_inner_ref())?;
         Ok(Self { public_key })
     }
 
